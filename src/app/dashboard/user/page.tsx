e42d86aa58b2d46298e87eb98c096ca0e6670d51
masterpeace <masterpeace@gmail.com>
2025-02-27 17:14:16 +0530

    hello function  btn

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,5 +1,5 @@
 // User component
 const User: React.FC = () => {
-  return <div> user lekkdfdsfhdfdfvdvfdjvfjdfvdhbfhdfbsdfbhs</div>;
+  return <div> user lekkdfdsfhdfdfvdvfdjvfjdfvdhbfhdfbsdfbhs<button>hello function</button></div>;
 };
 export default User;
